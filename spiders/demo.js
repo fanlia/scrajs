8d9c6d88731ad22aa487a5a7355a32b12ea8669f
odootest <odootest2023@163.com>
2023-11-17 09:17:31 +0800

    refine

--- spiders/demo.js
+++ spiders/demo.js
@@ -1,5 +1,6 @@
 
-const query = `
+export const spiders = [
+`
 query q(
   $url: JSON!
 ) {
@@ -13,43 +14,7 @@ query q(
     next:url(selector: ".next a")
   }
 }
-`
-
-const parse = async function* (response) {
-
-  const data = response.data.page
-
-  for (const d of data.children) {
-    yield { type: 'data', data: d }
-  }
-  
-  const nextUrl = data.next
-  if (nextUrl) {
-    yield {
-      type: 'request',
-      data: {
-        name: 'quotes',
-        query,
-        variables: {
-          url: nextUrl,
-        },
-        parse,
-      },
-    }
-  }
-}
-
-export const spiders = async () => [
-  {
-    name: 'quotes',
-    query,
-    variables: {
-      url: 'http://quotes.toscrape.com/tag/humor/',
-    },
-    parse,
-  },
+`,
 ]
 
-export const workers = [
-  'log',
-]
+export const url = 'http://quotes.toscrape.com/tag/humor/'
